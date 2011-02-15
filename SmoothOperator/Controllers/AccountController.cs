674d6434c8fda7b5b89206eab8a2a1d39bfec0db
Donn <dfelker@ally.com>
2011-02-15 16:02:46 -0600

    Initial checkin

---
+++ SmoothOperator/Controllers/AccountController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using SmoothOperator.Models;
+
+namespace SmoothOperator.Controllers
+{
+
+    [HandleError]
+    public class AccountController : Controller
+    {
+
+        public IFormsAuthenticationService FormsService { get; set; }
+        public IMembershipService MembershipService { get; set; }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (FormsService == null) { FormsService = new FormsAuthenticationService(); }
+            if (MembershipService == null) { MembershipService = new AccountMembershipService(); }
+
+            base.Initialize(requestContext);
+        }
+
+        // **************************************
+        // URL: /Account/LogOn
+        // **************************************
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (MembershipService.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsService.SignIn(model.UserName, model.RememberMe);
+                    if (!String.IsNullOrEmpty(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        // **************************************
+        // URL: /Account/LogOff
+        // **************************************
+
+        public ActionResult LogOff()
+        {
+            FormsService.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // **************************************
+        // URL: /Account/Register
+        // **************************************
+
+        public ActionResult Register()
+        {
+            ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel x)
+        {
+            if (ModelState.IsValid)
+            {
+                this.ControllerContext.HttpContext.Response.StatusCode = (int)System.Net.HttpStatusCode.OK; 
+                return Json("Awesome!"); 
+            }
+
+            this.ControllerContext.HttpContext.Response.StatusCode = (int)System.Net.HttpStatusCode.NotAcceptable;
+            return Json("Fail!"); 
+        }
+
+        // **************************************
+        // URL: /Account/ChangePassword
+        // **************************************
+
+        [Authorize]
+        public ActionResult ChangePassword()
+        {
+            ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePasswordModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (MembershipService.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword))
+                {
+                    return RedirectToAction("ChangePasswordSuccess");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            ViewData["PasswordLength"] = MembershipService.MinPasswordLength;
+            return View(model);
+        }
+
+        // **************************************
+        // URL: /Account/ChangePasswordSuccess
+        // **************************************
+
+        public ActionResult ChangePasswordSuccess()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SmoothOperator/HeaderValueProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmoothOperator
+{
+    public class HeaderValueProviderFactory : ValueProviderFactory
+    {
+        public class HeaderValueProvider : IValueProvider
+        {
+            private readonly NameValueCollection _headers;
+
+            public HeaderValueProvider(NameValueCollection headers)
+            {
+                _headers = headers;
+            }
+
+            public bool ContainsPrefix(string prefix)
+            {
+                return _headers.AllKeys.Any(x => x.Contains(prefix)); 
+            }
+
+            public ValueProviderResult GetValue(string key)
+            {
+                if (_headers == null)
+                {
+                    return null;
+                }
+                
+                var val = _headers.GetValues(key) == null ? null : _headers.GetValues(key).FirstOrDefault();
+                return val != null
+                           ? new ValueProviderResult(val, val, CultureInfo.CurrentCulture)
+                           : null;
+            }
+        }
+
+        public override IValueProvider GetValueProvider(ControllerContext controllerContext)
+        {
+            return new HeaderValueProvider(controllerContext.HttpContext.Request.Headers); 
+        }
+    }
+    
+}
